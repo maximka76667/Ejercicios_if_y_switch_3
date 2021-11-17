8750ccd07a234d5d836960e4643cda9d025ce7d6
Maxim <74974283+maximka76667@users.noreply.github.com>
2021-11-17 12:36:28 +0100

    ej5

--- src/ej5/Ej5.java
+++ src/ej5/Ej5.java
@@ -1,8 +1,11 @@
 package ej5;
 
 import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class Ej5 {
 
@@ -11,13 +14,26 @@ public class Ej5 {
 
 		System.out.print("Precio: ");
 		double precio = entrada.nextDouble();
+		
+		SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
 
-		System.out.print("Fecha de nacimiento (dd/mm/aaaa): ");
+		System.out.print("Fecha de nacimiento (dd.mm.aaaa): ");
 		String fecha_nacimiento = entrada.nextLine();
 		
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//		String fecha_de_hoy = formatter.format(LocalDateTime.now());
+		
+		Date date_de_nacimiento = new Date();
+		try {
+			date_de_nacimiento = format.parse(fecha_nacimiento);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Date fecha_de_hoy = new Date();
 
-		System.out.println("" + formatter.format(LocalDateTime.now()));
+		System.out.println((date_de_nacimiento.getTime() - fecha_de_hoy.getTime()) / 24);
 		entrada.close();
 	}
 
